f0ad24f231ec96783b019867269cf75834214553
Richard <cx_akns@hotmail.com>
2014-12-29 22:47:47 +0800

    add Palindrome Number

---
+++ palindrome-number/Solution.cpp
@@ -0,0 +1,43 @@
+
+class Solution
+{
+public:
+	int calcDigit(int x)
+	{
+		int cnt = 1;
+		while (x >= 10)
+		{
+			x = x / 10;
+			cnt++;
+		}
+		return cnt;
+	}
+
+	bool isPalindrome(int x)
+	{
+
+		if (x < 0)
+		{
+			return false;
+		}
+		int max_digit = calcDigit(x);
+		while (max_digit > 0)
+		{
+
+			if (max_digit == 1)
+			{
+				return true;
+			}
+
+			int max_value = x / std::pow(10, max_digit - 1);
+			int min_value = x % 10;
+			if (max_value != min_value)
+			{
+				return false;
+			}
+			x = (x - max_value * std::pow(10, max_digit - 1)) / 10;
+			max_digit -= 2;
+		}
+		return true;
+	}
+};
